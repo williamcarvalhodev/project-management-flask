9953aa66959e6fccbc9629ef6a362fda73a49c69
William Carvalho <williamcarvalhowebsites@gmail.com>
2026-08-27 09:03:16 +0000

    add app route structure

--- app.py
+++ app.py
@@ -1,2 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/project_tracker")
+def project_tracker():
+    return render_template("project_tracker.html")
+
+
+@app.route("/in_progress")
+def in_progress():
+    return render_template("in_progress.html")
+
+
+@app.route("completed_projects")
+def completed_projects():
+    return render_template("completed_projects.html")   
+
+
+if __name__ == "__main__":
+    app.run(debug=True)   
 
